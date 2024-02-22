5b2bf4fb8ad3b41545efafb5abd46061f4084929
NAYELI MELISSA URRUTIA ORELLANA <nurrutiao@miumg.edu.gt>
2024-02-21 22:24:51 -0600

    Se agrego el caso para calificar la zona minima

--- Tarea_3.12/Program.cs
+++ Tarea_3.12/Program.cs
@@ -13,28 +13,44 @@ namespace Tarea_3._12
             //PROGRAMA 12
  
             double calificacion;
+            double zona;
+            Console.WriteLine("Ingrese la Nota de su Zona 0-65");
+            zona = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Ingrese la Nota de su Examen");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            switch (zona)
+            {
+                case >= 30 and <= 35:
+                    Console.WriteLine("Excelente");
+                    break;
+                case >= 0 and <= 26:
+                    Console.WriteLine("No llego a Zona Minima");
+
+                default:
+                    Console.WriteLine("Calificacion No válida");
+                    break;
+            }
+                    Console.ResetColor();
+
+            
+
+            Console.WriteLine("Ingrese la Nota de su Examen Final de 0-35");
             calificacion = double.Parse(Console.ReadLine());
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             switch (calificacion)
             {
-                case >= 89 and <= 100:
+                case >= 30 and <= 35:
                     Console.WriteLine("Excelente");
                     break;
-                case >= 80 and <= 88:
+                case >= 20 and <= 29:
                     Console.WriteLine("Muy Bien");
                     break;
-                case >= 69 and <= 79:
+                case >= 10 and <= 19:
                     Console.WriteLine("Bueno");
                     break;
-                case >= 60 and <= 68:
+                case >= 0 and <= 9:
                     Console.WriteLine("Tiene que Mejorar");
-                    break;
-
-                case >= 50 and <= 59:
-                    Console.WriteLine("Reprovado");
                     break;
 
                 default:
@@ -43,6 +59,10 @@ namespace Tarea_3._12
 
 
             }
+
+            double notf = zona + calificacion;
+
+            Console.WriteLine("Su nota Final es de: " + notf);
             Console.ResetColor();
             Console.ReadKey();
 
