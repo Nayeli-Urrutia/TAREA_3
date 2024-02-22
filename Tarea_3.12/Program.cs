67de9fec349a676b6415be3d7e9ab9b406ef75d1
NAYELI MELISSA URRUTIA ORELLANA <nurrutiao@miumg.edu.gt>
2024-02-21 23:34:55 -0600

    Se agrego una sentencia if para evaluar si la calificacion es un número entero, ademas se agrego la suma de estos para la calificacion final

--- Tarea_3.12/Program.cs
+++ Tarea_3.12/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,58 +12,81 @@ namespace Tarea_3._12
         static void Main(string[] args)
         {
             //PROGRAMA 12
- 
+           
             double calificacion;
             double zona;
             Console.WriteLine("Ingrese la Nota de su Zona 0-65");
             zona = double.Parse(Console.ReadLine());
 
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            switch (zona)
+            if (zona is int)
             {
-                case >= 30 and <= 35:
-                    Console.WriteLine("Excelente");
-                    break;
-                case >= 0 and <= 26:
-                    Console.WriteLine("No llego a Zona Minima");
-
-                default:
-                    Console.WriteLine("Calificacion No válida");
-                    break;
+                Console.WriteLine("El número es entero");
             }
-                    Console.ResetColor();
+            else
+            {
+                Console.WriteLine("El número no es entero");
+            }
 
-            
 
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            switch (zona)
+                    {
+                        case >= 30 and <= 65:
+                            Console.WriteLine("Excelente");
+                            break;
+                        case >= 0 and <= 26:
+                            Console.WriteLine("No llego a Zona Minima");
+                            break;
+
+                        default:
+                            Console.WriteLine("Calificacion No válida");
+                            break;
+                    }
+            Console.ResetColor();
+                
             Console.WriteLine("Ingrese la Nota de su Examen Final de 0-35");
             calificacion = double.Parse(Console.ReadLine());
 
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            switch (calificacion)
+            if (calificacion is int)
             {
-                case >= 30 and <= 35:
-                    Console.WriteLine("Excelente");
-                    break;
-                case >= 20 and <= 29:
-                    Console.WriteLine("Muy Bien");
-                    break;
-                case >= 10 and <= 19:
-                    Console.WriteLine("Bueno");
-                    break;
-                case >= 0 and <= 9:
-                    Console.WriteLine("Tiene que Mejorar");
-                    break;
-
-                default:
-                    Console.WriteLine("Calificacion No válida");
-                    break;
-
-
+                Console.WriteLine("El número es entero");
+            }
+            else
+            {
+                Console.WriteLine("El número no es entero");
             }
 
-            double notf = zona + calificacion;
+            switch (calificacion)
+                    {
+                        case >= 30 and <= 35:
+                            Console.WriteLine("Excelente");
+                            break;
+                        case >= 20 and <= 29:
+                            Console.WriteLine("Muy Bien");
+                            break;
+                        case >= 10 and <= 19:
+                            Console.WriteLine("Bueno");
+                            break;
+                        case >= 0 and <= 9:
+                            Console.WriteLine("Tiene que Mejorar");
+                            break;
 
-            Console.WriteLine("Su nota Final es de: " + notf);
+                        default:
+                            Console.WriteLine("Calificacion No válida");
+                            break;
+                    }
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            double final = zona + calificacion;
+
+            if (final> 61)
+            {
+                Console.WriteLine("Esta Aprobado!");
+            }
+            else
+            {
+                Console.WriteLine("Ha Reprobado");
+            }
             Console.ResetColor();
             Console.ReadKey();
 
